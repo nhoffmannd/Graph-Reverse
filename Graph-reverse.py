a1bb3a789b774e2addb682f053da1b40e7acf62e
Nico <inphoffmann@gmail.com>
2019-05-10 17:15:34 -0300

    Hay que arreglar detector.
    Poner las definiciones en otro archivo.

--- Graph-reverse.py
+++ Graph-reverse.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 import sys
 import PIL
 import numpy
+debug = True
 
 class Example(QMainWindow):
     ##Esto es estándar de PyQt5.
@@ -24,7 +25,7 @@ class Example(QMainWindow):
         self.label = QTextEdit()
         self.setCentralWidget(self.label)
 
-        ##Definimos funciones mas sencillamente.        
+        ##Definimos funciones mas sencillamente.
         openFile    = new_function('web.png', 'Open', 'Ctrl+O', 'Abre', self, fileMenu)
         exitProgram = new_function('web.png','Cerrar','Ctrl+X', 'Exit', self, fileMenu)
         openFile.triggered.connect(self.open_file)
@@ -39,15 +40,34 @@ class Example(QMainWindow):
         ##Las funciones más sencillas primero.
 
     def open_file(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', "", "Image Files (*.jpg, *.bmp, *.png)")
-        workfile = misc.imread('face.png')
-        mi = workfile.shape[1]
-        histogram = numpy.full((mi,1),0)
-        ##ESTO ANDA
-        for a in range (0, mi):
-            histogram(a, 1) = workfile[]
-        
+        fname = QFileDialog.getOpenFileName(self, 'Open file', "", "Image Files (*.jpg *.bmp *.png)")
+        workfile = misc.imread(fname[0])
+        col = workfile.shape[0]
+        row = workfile.shape[1]
+        histogram = numpy.full((col),0)
+        for a in range (0, col):
+            for b in range (0, row):
+                histogram[a]=histogram[a]+max(workfile[a][b][:])
 
+        vid = int(col/2)
+        histmin = min(histogram[0:vid])
+        left = 0
+        for a in range (0, vid):
+            if ((abs(histmin/histogram[a])>0.95) and (abs(histmin/histogram[a])<1.05)):
+                left = a
+                break
+
+        histmin = min(histogram[vid:col])
+        right = col
+        for a in range (vid, col):
+            print(a)
+##            if ((abs(histmin/histogram[a])>0.95) and (abs(histmin/histogram[a])<1.05)):
+##                right = a
+##                break
+            
+        print (left)
+        print (right)
+            
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
